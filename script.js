1d27c7389951c8b828b67cf71ae65cc9f480d43d
Mahmud <mahmudcoder777@gmail.com>
2026-03-11 10:25:45 +0300

    select ga ozgina ozgartirdim

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const reset = document.getElementById("reset");
 const hisob = document.getElementById("hisob");
 const count = document.getElementById("count");
 const select = document.querySelectorAll("select");
+const result = document.querySelector(".result");
+const ism = document.getElementById("asmaulhusna");
+const z = document.getElementById("z")
 
 let a = 0;
 count.addEventListener("click", function () {
@@ -27,3 +30,9 @@ select.forEach((function (sel) {
         hisob.textContent = a
     })
 }))
+ism.addEventListener("change",()=>{
+    result.textContent = ism.value
+})
+z.addEventListener("change",function(){
+    result.textContent = z.value
+})
